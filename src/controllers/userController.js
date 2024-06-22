733f52537cd1258ee3626a652de3b85d80934736
ktruon01 <kevintruong.11235@gmail.com>
2024-06-21 23:34:37 -0400

    Configure Docker for development. Create Mongo container

--- src/controllers/userController.js
+++ src/controllers/userController.js
@@ -4,6 +4,7 @@ const User = require('../models/userModel');
 exports.getUsers = async (req, res) => {
     try {
         const users = await User.find();
+        console.log(`Retrieved ${users.length} users`); // Console log the number of users retrieved
         res.status(200).json(users);
     } catch (error) {
         res.status(500).json({ message: error.message });
