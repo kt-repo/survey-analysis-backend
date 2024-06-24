139e2fa4441da35e311e1d324d540d83ba0b4ca5
ktruon01 <kevintruong.11235@gmail.com>
2024-06-23 20:55:12 -0400

    Remove mongo warnings

--- src/app.js
+++ src/app.js
@@ -19,7 +19,7 @@ app.use('/api/learning-methods', learningMethodRoutes);
 app.use('/api/technologies-adopted', technologyAdoptedRoutes);
 
 // connect to database
-mongoose.connect(config.dbConnectionString, { useNewUrlParser: true, useUnifiedTopology: true })
+mongoose.connect(config.dbConnectionString)
     .then(() => console.log('Connected to MongoDB'))
     .catch(err => console.error('Could not connect to MongoDB', err));
 
